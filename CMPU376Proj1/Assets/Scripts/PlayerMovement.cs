ec94e8fd03f0512159fedfbfa878c573fd4acaf4
kellyyu5195 <keyu@vassar.edu>
2015-09-20 21:46:16 -0400

    collisions working

---
+++ CMPU376Proj1/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float speed = 1f;
+
+	Rigidbody playerRB;
+	float movement;
+
+	// Use this for initialization
+	void Start () {
+		playerRB = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		
+		Vector3 movement = new Vector3 (moveHorizontal, moveVertical, 0.0f);
+		playerRB.velocity = movement * speed;
+	}
+}
