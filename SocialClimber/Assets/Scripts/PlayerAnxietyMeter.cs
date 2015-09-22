78cda7d98de8d19a6cc546b2f780e89f845f2ee3
kellyyu5195 <keyu@vassar.edu>
2015-09-22 01:21:25 -0400

    playing around

---
+++ SocialClimber/Assets/Scripts/PlayerAnxietyMeter.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerAnxietyMeter : MonoBehaviour {
+
+	public float startingAnxiety;
+	public float currentAnxiety;
+	public float startingFriends;
+	public float currentFriends;
+	public Slider anxietySlider;
+	public Slider friendSlider;
+
+	void Awake ()
+	{
+		currentAnxiety = startingAnxiety;
+		currentFriends = startingFriends;
+	}
+
+
+	public void TakeAnxiety (float amount)
+	{
+		currentAnxiety += amount;
+		anxietySlider.value = currentAnxiety;
+	}
+
+	public void TakeFriends (float amount)
+	{
+		currentFriends += amount;
+		friendSlider.value = currentFriends;
+	}
+      
+}
---
+++ SocialClimber/Assets/Scripts/PlayerNPCInteraction.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerNPCInteraction : MonoBehaviour {
+
+	private Animator anim;
+	private GameObject HUDCanvas;
+	private PlayerMovement playerMovement;
+	private PlayerAnxietyMeter playerAnxiety;
+	private KeyCode key;
+	private float friendAmt;
+	private float anxietyAmt;
+
+	// Use this for initialization
+	void Start () {
+		HUDCanvas = GameObject.FindGameObjectWithTag ("HUDCanvas");
+		playerMovement = GetComponent<PlayerMovement> ();
+		anim = HUDCanvas.GetComponent<Animator> ();
+		playerAnxiety = GetComponent<PlayerAnxietyMeter> ();
+	}
+
+	void Update() {
+		setAmts ();
+	}
+
+	void OnTriggerEnter (Collider other) {
+		if (other.tag == "OtherStudent") {
+			playerMovement.enabled = false;
+			anim.SetTrigger ("Interact");
+			playerAnxiety.TakeFriends (friendAmt);
+			playerAnxiety.TakeAnxiety (anxietyAmt);
+		}
+		playerMovement.enabled = true;
+	}
+
+
+	void setAmts() {
+		if (Input.GetKey (KeyCode.Alpha1)) {
+			friendAmt = 0;
+			anxietyAmt = 1;
+			anim.SetTrigger ("Back");
+			Debug.Log("blah");
+		} else {
+			if (Input.GetKey (KeyCode.Alpha2) || Input.GetKey (KeyCode.Alpha3)) {
+				friendAmt = Random.Range (0, 10) % 2;
+				anim.SetTrigger ("Back");
+				anxietyAmt = 1;
+			} else {
+				friendAmt = 0;
+				anxietyAmt = 0;
+			}
+		}
+	}
+
+}
