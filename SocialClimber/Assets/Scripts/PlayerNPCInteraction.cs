ad134891e0c73769cbd1d1e17fecc1d2784cf613
kellyyu5195 <keyu@vassar.edu>
2015-09-22 13:54:45 -0400

    things r not lookin up

--- SocialClimber/Assets/Scripts/PlayerNPCInteraction.cs
+++ SocialClimber/Assets/Scripts/PlayerNPCInteraction.cs
@@ -8,11 +8,11 @@ public class PlayerNPCInteraction : MonoBehaviour {
 	GameObject HUDCanvas;
 	PlayerMovement playerMovement;
 	PlayerAnxietyMeter playerAnxiety;
-	int thing;
+//	int thing;
 	float friendAmt;
 	float anxietyAmt;
-
-	// Use this for initialization
+	bool blah;
+	
 	void Start () {
 		player = GameObject.FindGameObjectWithTag ("Player");
 		HUDCanvas = GameObject.FindGameObjectWithTag ("HUDCanvas");
@@ -21,49 +21,62 @@ public class PlayerNPCInteraction : MonoBehaviour {
 		playerAnxiety = player.GetComponent<PlayerAnxietyMeter> ();
 	}
 
-	void FixedUpdate() {
+	void Update() {
 		setAmts ();
 	}
 
+	void setAmts() {
+//		if (blah) {
+			if (Input.GetKey (KeyCode.C)) {
+				playerAnxiety.TakeAnxiety (1);
+				playerAnxiety.TakeFriends (0);
+				anim.SetTrigger ("Back");
+				Debug.Log ("thing");
+//				blah = false;
+			} else {
+				if (Input.GetKey (KeyCode.A) || Input.GetKey (KeyCode.B)) {
+					playerAnxiety.TakeAnxiety (Random.Range (-1, 1));
+					playerAnxiety.TakeFriends (Random.Range (0, 1));
+					anim.SetTrigger ("Back");
+//					blah = false;
+				} else {
+					playerAnxiety.TakeAnxiety (0);
+					playerAnxiety.TakeFriends (0);
+//					blah = false;
+				}
+			}
+		}
+//	}
+
+	
 	void OnTriggerEnter (Collider other) {
 		if (other.tag == "Player") {
 			playerMovement.enabled = false;
 			anim.SetTrigger ("Interact");
-			doSliderThings();
+//			doSliderThings();
+			blah = true;
 		}
 		playerMovement.enabled = true;
 	}
 
-
-	void setAmts() {
-		if (Input.GetKey (KeyCode.C)) {
-			thing = 0;
-		} else {
-			if (Input.GetKey (KeyCode.A) || Input.GetKey (KeyCode.B)) {
-				thing = 1;
-			} else {
-				thing = 2;
-			}
-		}
-	}
-
-	void doSliderThings () {
-		switch (thing) {
-		case 0:
-			friendAmt = 0f;
-			anxietyAmt = 1f;
-			break;
-		case 1:
-			friendAmt = Random.Range (0, 1);
-			anxietyAmt = Random.Range (-1, 1);
-			break;
-		case 2:
-			friendAmt = 0f;
-			anxietyAmt = 0f;
-			break;
-		}
-		playerAnxiety.TakeFriends (friendAmt);
-		playerAnxiety.TakeAnxiety (anxietyAmt);
-	}
+//	void doSliderThings () {
+////		switch (thing) {
+////		case 0:
+////			friendAmt = 0f;
+////			anxietyAmt = 1f;
+////			break;
+////		case 1:
+////			friendAmt = Random.Range (0, 1);
+////			anxietyAmt = Random.Range (-1, 1);
+////			anim.SetTrigger("Back");
+////			break;
+////		case 2:
+////			friendAmt = 0f;
+////			anxietyAmt = 0f;
+////			break;
+////		}
+//		playerAnxiety.TakeFriends (friendAmt);
+//		playerAnxiety.TakeAnxiety (anxietyAmt);
+//	}
 
 }
