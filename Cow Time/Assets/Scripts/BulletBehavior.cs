843607a34ef0d0e4f9904fb0e7c99eb0358fa2cc
kellyyu5195 <keyu@vassar.edu>
2015-10-26 21:32:11 -0400

    working on mom shootin

---
+++ Cow Time/Assets/Scripts/BulletBehavior.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletBehavior : MonoBehaviour {
+
+	Rigidbody2D rb;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+		rb.velocity = new Vector2(10, 0);
+	}
+
+	void OnCollisionEnter2D (Collision2D col) {
+		if (col.gameObject.tag == "heart") {
+			Destroy (col.gameObject);
+		}
+	}
+}
--- Cow Time/Assets/Scripts/MomShooting.cs
+++ Cow Time/Assets/Scripts/MomShooting.cs
@@ -4,10 +4,19 @@ using System.Collections;
 public class MomShooting : MonoBehaviour {
 
 	public GameObject bullet;
-	int timer;
+	int timer = 0;
+	Transform transform;
+
+	void Start() {
+		transform = GetComponent<Transform> ();
+	}
 
 	void Update() {
-
+		timer++;
+		if (timer == 500) {
+			Instantiate(bullet, transform.position, Quaternion.identity);
+			timer = 0;
+		}
 	}
 
 }
