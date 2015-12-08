f9e13f8ae810560ff795268174bc4f98bef35467
kellyyu5195 <keyu@vassar.edu>
2015-12-07 22:13:42 -0500

    working on pick up mechanics

---
+++ SleepDeprivation/Assets/Scripts/PickUp.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickUp : MonoBehaviour {
+
+	// private fields
+	Transform playerTrans;
+
+	//public fields
+	public Camera playerCam;
+	public Camera tableCam;
+
+	// Use this for initialization
+	void Start () {
+		playerTrans = GetComponent<Transform> ();
+		playerCam.enabled = true;
+		tableCam.enabled = false;
+	}
+
+	void OnTriggerEnter(Collision coll) {
+		GameObject obj = coll.gameObject;
+//		SupplyInfo info = obj.GetComponent<SupplyInfo>;
+		Transform objTrans = coll.gameObject.transform;
+		if (obj.tag == "PickUp") {
+			playerCam.enabled = false;
+			tableCam.enabled = true;
+			playerTrans.position = new Vector3 (0, 0, 0);
+//			objTrans.position = SupplyInfo.loc;
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown("space")) {
+			playerCam.enabled = true;
+			tableCam.enabled = false;
+		}
+	
+	}
+}
