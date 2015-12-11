7611808f20188a24efba133be44a529fac668644
kellyyu5195 <keyu@vassar.edu>
2015-12-11 00:04:11 -0500

    Audio & Instructions loaded

---
+++ SleepDeprivation/Assets/Scripts/ClickCompu.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickCompu : MonoBehaviour {
+
+	public float timeBetweenBullets = 1.0f;
+	public float range = 100.0f;
+	public GameObject poster;
+	
+	float timer;
+	Ray shootRay;
+	int shootableMask;
+	float smallDiff = 0.01f;
+	Vector3 posterPos;
+	//	Transform playerTransform; 
+	
+	void Awake ()
+	{
+		shootableMask = LayerMask.GetMask ("Shootable");
+		//		playerTransform = GetComponent<Transform> ();
+	}
+	
+	
+	void FixedUpdate ()
+	{	
+		timer += Time.deltaTime;
+		
+		if (Input.GetButton ("Fire1") && timer > timeBetweenBullets) {
+			shootRay = Camera.main.ScreenPointToRay (Input.mousePosition);
+			RaycastHit shootHit;
+			if (Physics.Raycast (shootRay, out shootHit, range, shootableMask)) {
+				if (shootHit.collider.tag == "canTag") {
+					Transform hitTransform = shootHit.transform;
+					Quaternion rotation = hitTransform.rotation;
+					//					posterPos.x = shootHit.point.x + smallDiff;
+					//					posterPos.y = shootHit.point.y + smallDiff;
+					//					posterPos.z = shootHit.point.z + smallDiff;
+					posterPos = shootHit.point;
+					Instantiate (poster, posterPos, rotation);
+					timer = 0;
+				}
+			}
+		}
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ SleepDeprivation/Assets/Scripts/Sounds.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sounds : MonoBehaviour {
+	
+	public AudioClip groan;
+	public AudioClip goToSleep;
+	// Use this for initialization
+	IEnumerator Start () {
+		AudioSource audio = GetComponent<AudioSource>();
+		audio.Play();
+		yield return new WaitForSeconds(audio.clip.length);
+		audio.clip = groan;
+		audio.Play();
+		yield return new WaitForSeconds(audio.clip.length);
+		audio.clip = goToSleep;
+		audio.Play();
+	}
+}
